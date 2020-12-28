e271242c8d98f475700f745d5961a81b13edb764
Владимир <Stasfatgrowler@yandex.ru>
2020-12-28 22:36:26 +1000

    added Ingredient model, Recipe model and RecipeIngredient model

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,13 +1,35 @@
 from django.contrib import admin
 
-from .models import Ingredient
+from .models import Ingredient, Recipe, RecipeIngredient
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
 
 
 class IngredientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'unit')
+    list_display = ('name', 'unit',)
     search_fields = ('name',)
     list_filter = ('name',)
     empty_value_display = '-пусто-'
 
 
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = (RecipeIngredientInline, )
+    list_display = ('title', 'author',)
+    search_fields = ('title',)
+    list_filter = ('title',)
+    empty_value_display = '-пусто-'
+
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('ingredient', 'recipe', 'amount',)
+    search_fields = ('ingredient',)
+    list_filter = ('ingredient',)
+    empty_value_display = '-пусто-'
+
+
 admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -1,10 +1,42 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
+
+# verbose_name, related_name
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=255, verbose_name='ingredient name')
     unit = models.CharField(max_length=64,
-                            verbose_name='ingredient unit')  # проверить
+                            verbose_name='ingredient unit')
 
     def __str__(self):
         return f'{self.pk} - {self.name} - {self.unit}'
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='author_recipes')
+    title = models.CharField(max_length=255)
+    # image = models.ImageField(upload_to='recipe/', blank=True,
+    #                           null=True)
+    description = models.TextField(blank=True,
+                                   verbose_name='recipe description')
+    ingredients = models.ManyToManyField(Ingredient,
+                                         through='RecipeIngredient')
+    # tags =
+    cooking_time = models.IntegerField()
+    # slug = models.SlugField(max_length=200, unique=True)
+    pub_date = models.DateTimeField(verbose_name='date published',
+                                    auto_now_add=True,
+                                    db_index=True)
+
+    def __str__(self):
+        return f'{self.pk} - {self.title} - {self.author}'
+
+
+class RecipeIngredient(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    amount = models.IntegerField()
